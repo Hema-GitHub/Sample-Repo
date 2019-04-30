06a30512ca1db5b1cbbb14ba287bcfc30862a792
Jothipriya karunanithi <jkarunanithi@vmware.com>
2019-04-30 14:29:06 +0530

    commit

--- testproject/src/testproject/ascending.java
+++ testproject/src/testproject/ascending.java
@@ -10,6 +10,7 @@ public class ascending {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// added comment
 		Scanner in = new Scanner(System.in);
 		
 		// Getting numbers from user
